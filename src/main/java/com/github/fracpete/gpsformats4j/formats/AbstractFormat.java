2d5bf4aebd7d00db806fe340d83b14521fb3a83f
Peter Reutemann <fracpete@gmail.com>
2016-12-29 14:27:39 +1300

    defined interface for formats

--- src/main/java/com/github/fracpete/gpsformats4j/formats/AbstractFormat.java
+++ src/main/java/com/github/fracpete/gpsformats4j/formats/AbstractFormat.java
@@ -1,5 +1,58 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * AbstractFormat.java
+ * Copyright (C) 2016 FracPete
+ */
 package com.github.fracpete.gpsformats4j.formats;
+
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
+import java.util.List;
 
 public abstract class AbstractFormat {
 
+  /**
+   * Returns whether reading is supported.
+   *
+   * @return		true if supported
+   */
+  public abstract boolean canRead();
+
+  /**
+   * Reads the file.
+   *
+   * @param input	the input file
+   * @return		the collected data
+   */
+  public abstract List<CSVRecord> read(File input);
+
+  /**
+   * Returns whether writing is supported.
+   *
+   * @return		true if supported
+   */
+  public abstract boolean canWrite();
+
+  /**
+   * Writes to a file.
+   *
+   * @param data	the data to write
+   * @param output	the output file
+   */
+  public abstract String write(List<CSVRecord> data, File output);
 }
